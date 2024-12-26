40e8d10e407cfe4f54b215369afd93381037bec3
Mishell Yagual Mendoza <mishell.yagual@sofka.com.co>
2024-12-26 06:09:44 -0500

    Use of the gateway to send the messsage on this usecase

--- domain/usecases/src/main/java/ec/com/sofka/GetAccountByIdUseCase.java
+++ domain/usecases/src/main/java/ec/com/sofka/GetAccountByIdUseCase.java
@@ -1,16 +1,23 @@
 package ec.com.sofka;
 
 import ec.com.sofka.gateway.AccountRepository;
+import ec.com.sofka.gateway.BusMessage;
 
+//8. Set the use case to send a message to the bus
 public class GetAccountByIdUseCase{
 
     private final AccountRepository repository;
+    //Set the port
+    private final BusMessage busMessage;
 
-    public GetAccountByIdUseCase(AccountRepository repository) {
+    public GetAccountByIdUseCase(AccountRepository repository, BusMessage busMessage) {
         this.repository = repository;
+        this.busMessage = busMessage;
     }
 
     public Account apply(String id){
+        //Send a message to the bus
+        busMessage.sendMsg("Getting account by id: " + id);
         return repository.findByAcccountId(id);
     }
 }
